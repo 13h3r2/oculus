246e7b532744d0906e95e4d05828b81d32055ebb
Alexey Romanchuk <romanchuk@se-b.ru>
2011-09-02 17:42:35 +0700

    Highlight based on connection count

--- src/oculus/Database.py
+++ src/oculus/Database.py
@@ -1,17 +1,20 @@
 import time
 import pickle
-import logging
 import cx_Oracle
+
+stubmode = 0
 
 class DatabaseStorage:
     def __init__(self):
-        self.databases = [
-            DatabaseInfo("mox04.sibirenergo-billing.ru", "qaasr", "SYS", "*"),
-            DatabaseInfo("mox05.sibirenergo-billing.ru", "prodasr", "SYS", "*"),
-        ]
-#        f = open("oculus/databases.pickle", 'rb+');
-#        self.databases = pickle.load(f);
-#        f.close();
+        if stubmode == 1:
+            self.databases = [
+                DatabaseInfo("mox04.sibirenergo-billing.ru", "qaasr", "SYS", "*"),
+                DatabaseInfo("mox05.sibirenergo-billing.ru", "prodasr", "SYS", "*"),
+            ]
+        else:
+            f = open("oculus/databases.pickle", 'rb+');
+            self.databases = pickle.load(f);
+            f.close();
         
     def gather(self):
         return self.databases;
@@ -31,27 +34,29 @@ class DatabaseInfo(object):
         self.sys_password = sys_password
         
     def gather(self):
-        time.sleep(1);
-        self.schemes = [
-            SchemeInfo('DEV_RENAT', '32GB', 4),
-            SchemeInfo('DEV_ROMANCHUK', '31GB', 6 ),
-            SchemeInfo('TRUNK_STABLE_QA', '30GB', 0),
-        ]
-#        connection = cx_Oracle.connect(self.sys_login, self.sys_password, cx_Oracle.makedsn(self.host, 1521, self.sid), cx_Oracle.SYSDBA)        
-#        cursor = connection.cursor()
-#        cursor.execute('''
-#select owner, ''||trunc(sum(bytes)/1024/1024/1024, 1), nvl(c_count, 0)
-#from dba_segments dbs
-#left outer join ( select count(*) as c_count, s.username as username from v$session s group by s.username) s on s.username = dbs.owner
-#group by owner, c_count
-#having sum(bytes) > 5000000000
-#order by sum(bytes) desc
-#        ''')
-#        self.schemes = []
-#        for record in cursor.fetchall():
-#            self.schemes.append(SchemeInfo(record[0], record[1], record[2]))
-#        cursor.close()
-#        connection.close()
+        if stubmode == 1:
+            time.sleep(1);
+            self.schemes = [
+                SchemeInfo('DEV_RENAT', '32GB', 4),
+                SchemeInfo('DEV_ROMANCHUK', '31GB', 6 ),
+                SchemeInfo('TRUNK_STABLE_QA', '30GB', 0),
+            ]
+        else: 
+            connection = cx_Oracle.connect(self.sys_login, self.sys_password, cx_Oracle.makedsn(self.host, 1521, self.sid), cx_Oracle.SYSDBA)        
+            cursor = connection.cursor()
+            cursor.execute('''
+    select owner, ''||trunc(sum(bytes)/1024/1024/1024, 1), nvl(c_count, 0)
+    from dba_segments dbs
+    left outer join ( select count(*) as c_count, s.username as username from v$session s group by s.username) s on s.username = dbs.owner
+    group by owner, c_count
+    having sum(bytes) > 5000000000
+    order by sum(bytes) desc
+            ''')
+            self.schemes = []
+            for record in cursor.fetchall():
+                self.schemes.append(SchemeInfo(record[0], record[1], record[2]))
+            cursor.close()
+            connection.close()
         return self.schemes
         
         
